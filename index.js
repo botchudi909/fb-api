a46c4740cf7c25ec25c1942898054c0d93000db6
botchudi909 <botchudi909@gmail.com>
2026-07-25 07:27:40 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const axios = require("axios");
 
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
@@ -8,41 +10,86 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.json({
     status: true,
-    author: "TARIF",
-    message: "API Running"
-  });
-});
-
-app.get("/ping", (req, res) => {
-  res.json({
-    status: true,
-    message: "Pong!"
-  });
-});
-
-app.get("/info", (req, res) => {
-  res.json({
-    name: "Simple API",
-    version: "1.0.0",
+    message: "Facebook UID API Running",
     author: "TARIF"
   });
 });
 
-app.get("/status", (req, res) => {
-  res.json({
-    status: "online",
-    uptime: process.uptime(),
-    time: new Date()
-  });
+
+// UID Finder
+app.get("/uid", async (req, res) => {
+  try {
+
+    const input = req.query.user;
+
+    if (!input) {
+      return res.json({
+        status: false,
+        message: "Give Facebook username or profile link"
+      });
+    }
+
+
+    // Facebook ID extract method
+    let url = input;
+
+    if (!url.startsWith("http")) {
+      url = "https://www.facebook.com/" + url;
+    }
+
+
+    const response = await axios.get(url, {
+      headers: {
+        "User-Agent":
+        "Mozilla/5.0"
+      }
+    });
+
+
+    const html = response.data;
+
+
+    let uid =
+    html.match(/"userID":"(\d+)"/);
+
+
+    if (!uid) {
+      uid =
+      html.match(/profile_id=(\d+)/);
+    }
+
+
+    if (uid) {
+
+      return res.json({
+        status:true,
+        uid: uid[1],
+        profile: input
+      });
+
+    }
+
+
+    res.json({
+      status:false,
+      message:"UID not found"
+    });
+
+
+  } catch(e){
+
+    res.json({
+      status:false,
+      error:e.message
+    });
+
+  }
+
 });
 
-app.use((req, res) => {
-  res.status(404).json({
-    status: false,
-    message: "Endpoint not found"
-  });
-});
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT,()=>{
+ console.log(
+ `Server running on ${PORT}`
+ );
 });
